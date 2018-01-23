e3da33b6b15d989ba38d47a2ade7ef3b08f549a7
lorenzo stanton <lstanii54@gmail.com>
2018-01-22 21:42:55 -0800

    Updated var name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = function (app) {
+module.exports = function (express) {
     function ToolBelt() {
     }
 
-    ToolBelt.Router = require("./router.js")(app.Router());
+    ToolBelt.Router = require("./router.js")(express.Router());
     ToolBelt.Cluster = require("./cluster.js");
     return ToolBelt;
 };
